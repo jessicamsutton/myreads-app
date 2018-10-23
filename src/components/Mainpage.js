24188100ec6c0117045686b1bf79e8cd0ae9082b
Jessica Sutton <jmsutton0703@gmail.com>
2018-10-23 15:44:38 -0500

    Refactor Shelf rendering

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Link, Route } from 'react-router-dom'
-import Shelf from './Shelf.js'
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Shelf from './Shelf.js';
+import PropTypes from 'prop-types';
 
 class MainPage extends React.Component {
   render() {
@@ -8,15 +9,37 @@ class MainPage extends React.Component {
       <div className="list-books">
         <div className="list-books-content">
           <Route exact path="/" render={() => (
-            <Shelf />
+            <div>
+               <Shelf
+                  name={"Currently Reading"}
+                  id={"currentlyReading"}
+                  books={this.props.books}
+                  updateShelf={this.props.updateShelf}
+                />
+                <Shelf
+                  name={"Want to Read"}
+                  id={"wantToRead"}
+                  books={this.props.books}
+                  updateShelf={this.props.updateShelf}
+                />
+                <Shelf
+                  name={"Read"}
+                  id={"read"}
+                  books={this.props.books}
+                  updateShelf={this.props.updateShelf}
+                />
+            </div>
           )} />
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
-
+          <div className="open-search">
+            <Link to="/search">Add a book</Link>
+          </div>
         </div>
       </div>
-    )}
-  }
+  )}
+}
+
+MainPage.propTypes = {
+  books: PropTypes.array
+};
 
 export default MainPage
