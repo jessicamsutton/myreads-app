bcea71edc15fe8bb3399f085e3ac2f6d74d8e323
Jessica Sutton <jmsutton0703@gmail.com>
2018-10-23 20:02:19 -0500

    Check prop-types in book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,28 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import ShelfChanger from './ShelfChanger.js'
 
 class Book extends React.Component {
   render() {
     return (
       <div>
-        <li key={this.props.book.key}>
+        <li>
           <div className="book">
             <div className="book-top">
               <div className="book-cover"
                     style={{ width: 128, height: 193,
-                             backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+                             backgroundImage: `url("${this.props.url}")` }}></div>
                 <ShelfChanger
                   book={this.props.book}
+                  currentShelf={this.props.currentShelf}
                   updateShelf={this.props.updateShelf}
                 />
             </div>
-            <div className="book-title">{this.props.book.title}</div>
-            <div className="book-authors">{this.props.book.authors}</div>
+            <div className="book-title">{this.props.title}</div>
+            <div className="book-authors">{this.props.author}</div>
           </div>
         </li>
       </div>
     )
   }
 }
+
+Book.propTypes = {
+  currentShelf: PropTypes.func,
+  updateShelf: PropTypes.func,
+  url: PropTypes.string,
+  title: PropTypes.string,
+};
 
 export default Book
